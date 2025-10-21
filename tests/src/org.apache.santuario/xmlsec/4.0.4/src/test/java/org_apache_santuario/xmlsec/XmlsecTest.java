8e074679f58ef0020015ad0af8e97d10eb78a234
Mihailo Markovic <mihailo.markovic@oracle.com>
2025-10-21 22:57:47 +0200

    AI GEN Santuario test.

---
+++ tests/src/org.apache.santuario/xmlsec/4.0.4/src/test/java/org_apache_santuario/xmlsec/XmlsecTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_santuario.xmlsec;
+
+import org.apache.xml.security.Init;
+import org.apache.xml.security.algorithms.MessageDigestAlgorithm;
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.encryption.XMLCipher;
+import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.transforms.Transforms;
+import org.apache.xml.security.signature.ObjectContainer;
+import org.apache.xml.security.utils.Constants;
+import org.apache.xml.security.utils.EncryptionConstants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.ByteArrayOutputStream;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class XmlsecTest {
+
+    @BeforeAll
+    static void setup() {
+        Init.init();
+    }
+
+    @Test
+    void signAndVerifyEnvelopedSignature() throws Exception {
+        Document doc = newDocument();
+        Element root = doc.getDocumentElement();
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
+
+        XMLSignature signature = new XMLSignature(doc, "", XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA256);
+        root.appendChild(signature.getElement());
+
+        Transforms transforms = new Transforms(doc);
+        transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
+        transforms.addTransform(Transforms.TRANSFORM_C14N_EXCL_OMIT_COMMENTS);
+
+        signature.addDocument("", transforms, MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA256);
+        signature.addKeyInfo(kp.getPublic());
+        signature.sign(kp.getPrivate());
+
+        boolean valid = signature.checkSignatureValue(kp.getPublic());
+        assertThat(valid).isTrue();
+
+        NodeList sigNodes = doc.getElementsByTagNameNS(Constants.SignatureSpecNS, "Signature");
+        assertThat(sigNodes.getLength()).isGreaterThan(0);
+    }
+
+    @Test
+    void xmlEncryptionAesRoundTrip() throws Exception {
+        Document doc = newDocument();
+
+        KeyGenerator kg = KeyGenerator.getInstance("AES");
+        kg.init(128);
+        SecretKey secretKey = kg.generateKey();
+
+        Element data = (Element) doc.getElementsByTagName("data").item(0);
+        XMLCipher encCipher = XMLCipher.getInstance(XMLCipher.AES_128);
+        encCipher.init(XMLCipher.ENCRYPT_MODE, secretKey);
+        encCipher.doFinal(doc, data, true);
+
+        NodeList encrypted = doc.getElementsByTagNameNS(EncryptionConstants.EncryptionSpecNS, EncryptionConstants._TAG_ENCRYPTEDDATA);
+        assertThat(encrypted.getLength()).isEqualTo(1);
+
+        XMLCipher decCipher = XMLCipher.getInstance();
+        decCipher.init(XMLCipher.DECRYPT_MODE, secretKey);
+        decCipher.doFinal(doc, (Element) encrypted.item(0));
+
+        Element decryptedData = (Element) doc.getElementsByTagName("data").item(0);
+        assertThat(decryptedData).isNotNull();
+        assertThat(decryptedData.getTextContent()).isEqualTo("Hello World");
+    }
+
+    @Test
+    void canonicalizerInstanceAndCanonicalize() throws Exception {
+        Document doc = newDocument();
+
+        Canonicalizer canonicalizer = Canonicalizer.getInstance(Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        canonicalizer.canonicalizeSubtree(doc, baos);
+        byte[] canon = baos.toByteArray();
+
+        assertThat(canon).isNotNull();
+        assertThat(canon.length).isGreaterThan(0);
+    }
+
+    private static Document newDocument() throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document doc = dbf.newDocumentBuilder().newDocument();
+        Element root = doc.createElement("root");
+        Element data = doc.createElement("data");
+        data.setTextContent("Hello World");
+        root.appendChild(data);
+        doc.appendChild(root);
+        return doc;
+    }
+
+    @Test
+    void signAndVerifySameDocumentObjectReference() throws Exception {
+        Document doc = newDocument();
+        Element root = doc.getDocumentElement();
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
+
+        XMLSignature signature = new XMLSignature(doc, "", XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA256);
+        root.appendChild(signature.getElement());
+
+        ObjectContainer object = new ObjectContainer(doc);
+        object.setId("object1");
+
+        Element payload = doc.createElement("payload");
+        payload.setTextContent("Hello Same-Document Reference");
+        object.appendChild(payload);
+
+        signature.appendObject(object);
+
+        Transforms transforms = new Transforms(doc);
+        transforms.addTransform(Transforms.TRANSFORM_C14N_EXCL_OMIT_COMMENTS);
+
+        signature.addDocument("#object1", transforms, MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA256);
+        signature.addKeyInfo(kp.getPublic());
+        signature.sign(kp.getPrivate());
+
+        boolean valid = signature.checkSignatureValue(kp.getPublic());
+        assertThat(valid).isTrue();
+    }
+}
