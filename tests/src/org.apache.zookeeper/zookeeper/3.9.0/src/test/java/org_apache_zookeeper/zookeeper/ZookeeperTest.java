44cbd9ef72ba4a919af14d9101b2f45bff6487d4
Mihailo <mihailo.markovic@oracle.com>
2026-01-12 13:38:54 +0100

    Scaffold org.apache.zookeeper:zookeeper:3.9.0

---
+++ tests/src/org.apache.zookeeper/zookeeper/3.9.0/src/test/java/org_apache_zookeeper/zookeeper/ZookeeperTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_zookeeper.zookeeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class ZookeeperTest {
+    @Test
+    void test() throws Exception {
+        fail("TODO: Add test logic here");
+    }
+}
