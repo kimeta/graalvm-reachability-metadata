0a98a38562a2589bebc14c0542cfb566edcf5fd9
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-30 17:37:03 +0200

    Add Native Image support helper

---
+++ tests/tck-test-support/src/test/java/org/graalvm/internal/tck/NativeImageSupportTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.graalvm.internal.tck;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class NativeImageSupportTest {
+
+    @Test
+    void returnsFalseForNull() {
+        assertFalse(NativeImageSupport.isUnsupportedFeatureError(null));
+    }
+
+    @Test
+    void returnsFalseForOutOfMemoryError() {
+        assertFalse(NativeImageSupport.isUnsupportedFeatureError(new OutOfMemoryError()));
+    }
+
+    @Test
+    void returnsFalseForStackOverflowError() {
+        assertFalse(NativeImageSupport.isUnsupportedFeatureError(new StackOverflowError()));
+    }
+}
