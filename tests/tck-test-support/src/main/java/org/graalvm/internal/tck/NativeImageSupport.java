d83024a2d8a09f88fcb85d355945d50f58780808
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-30 17:37:03 +0200

    Add Native Image support helper

---
+++ tests/tck-test-support/src/main/java/org/graalvm/internal/tck/NativeImageSupport.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.graalvm.internal.tck;
+
+/// Utilities for handling Native Image runtime behavior in tests.
+public final class NativeImageSupport {
+
+    private static final String UNSUPPORTED_FEATURE_ERROR =
+            "com.oracle.svm.core.jdk.UnsupportedFeatureError";
+
+    private NativeImageSupport() {
+    }
+
+    /// Returns true when the error is an UnsupportedFeatureError thrown by
+    /// Native Image for unsupported dynamic operations such as runtime class loading.
+    public static boolean isUnsupportedFeatureError(Error error) {
+        return error != null
+                && UNSUPPORTED_FEATURE_ERROR.equals(error.getClass().getName());
+    }
+}
