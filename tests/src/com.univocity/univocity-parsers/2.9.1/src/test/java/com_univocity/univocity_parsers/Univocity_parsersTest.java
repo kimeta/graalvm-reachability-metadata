dbdcca3ac618e000498617273bae3db7b23ab86e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-01 22:40:33 +0200

    [GenAI] Add support for com.univocity:univocity-parsers:2.9.1 using gpt-5.5 (#4098)
    * Scaffold com.univocity:univocity-parsers:2.9.1
    
    * Dynamic-access coverage for com.univocity.parsers.annotations.helpers.AnnotationHelper (17/32)
    
    * Dynamic-access coverage for com.univocity.parsers.conversions.EnumConversion (23/32)
    
    * Dynamic-access coverage for com.univocity.parsers.annotations.helpers.FieldMapping (29/32)
    
    * Dynamic-access coverage for com.univocity.parsers.annotations.helpers.AnnotationRegistry$AnnotationAttributes (30/32)
    
    * Dynamic-access coverage for com.univocity.parsers.common.ArgumentUtils (31/32)
    
    * Dynamic-access coverage for com.univocity.parsers.common.processor.core.BeanConversionProcessor (32/32)
    
    * Update generated library support for com.univocity:univocity-parsers:2.9.1
    
    * Add support for com.univocity:univocity-parsers:2.9.1

---
+++ tests/src/com.univocity/univocity-parsers/2.9.1/src/test/java/com_univocity/univocity_parsers/Univocity_parsersTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_univocity.univocity_parsers;
+
+import org.junit.jupiter.api.Test;
+
+public class Univocity_parsersTest {
+    @Test
+    public void loadsLibrary() {
+        System.out.println("univocity-parsers is available");
+    }
+}
